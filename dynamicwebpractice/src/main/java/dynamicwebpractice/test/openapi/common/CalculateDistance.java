500fc8c2293688ec8a2c21e17e1c07b275619736
kimsolee <ahhasolee@naver.com>
2022-11-21 19:06:20 +0900

    거리 값 계산

--- dynamicwebpractice/src/main/java/dynamicwebpractice/test/openapi/common/CalculateDistance.java
+++ dynamicwebpractice/src/main/java/dynamicwebpractice/test/openapi/common/CalculateDistance.java
@@ -22,18 +22,21 @@ public class CalculateDistance {
 		    dist = Math.acos(dist);
 		    dist = rad2deg(dist);
 		    dist = dist * 60*1.1515*1609.344;
+		    
+		    dist = dist / 1000;
+		    dist = (Math.round(dist*10000)/10000.0); //소수 4자리까지 반올림한 값으로 변환
 			
-		    wifiInfolist.get(i).setDistance(dist / 1000);
+		    wifiInfolist.get(i).setDistance(dist);
 		}
 	
 	}
 	
-	//10진수를 radian(라디안)으로 변환
+	//10진수를 radian(라디안)으로 변환하기
 	public static double deg2rad(double deg){
 	    return (deg * Math.PI/180.0);
 	}
 	
-	//radian(라디안)을 10진수로 변환
+	//radian(라디안)을 10진수로 변환하기
 	public static double rad2deg(double rad){
 	    return (rad * 180 / Math.PI);
 	}
